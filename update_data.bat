9e00b9666e71381343f7bfadcecf4a425a58a18e
lorenzocotrina-cloud <lorenzocotrina@gmail.com>
2025-12-06 00:18:32 +0100

    Delete update_data.bat

--- update_data.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Descargando ultimos datos de partidos y estadisticas...
-npm run scrape
-echo.
-echo Datos actualizados correctamente!
-echo Recarga la pagina web para ver los cambios.
-pause
